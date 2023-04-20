a0832ce6f17ca08d58c9d652de1d880a8ada4b18
agusbw <agusbudhiarta81@gmial.com>
2023-04-20 23:36:26 +0800

    feat: add auth feature

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,3 +1,41 @@
-export default function index() {
-  return <h1>Dashboard Page</h1>;
+import heroImage from "../assets/hero.png";
+import herobg from "../assets/herobg.jpg";
+import { Link } from "react-router-dom";
+
+export default function Home() {
+  return (
+    <>
+      <div className="hero h-screen bg-hero">
+        <img
+          src={herobg}
+          alt="Hero Background"
+          className="object-cover  w-full h-screen opacity-50 lg:opacity-20"
+        />
+        <div className="hero-content flex-col  lg:flex-row-reverse">
+          <img src={heroImage} alt="Hero Image" className="hidden lg:block" />
+          <div>
+            <h1 className="text-5xl font-bold text-center lg:text-left">
+              <span className="text-secondary">Discover the Delights </span>
+              <br /> of Our Delicious Dishes
+            </h1>
+            <p className="py-6 text-center lg:text-left">
+              Welcome to{" "}
+              <span className="font-thin text-primary font-pacifico">
+                SpiceBox
+              </span>
+              , your go-to destination for all things culinary. We believe that
+              cooking should be a fun and enjoyable experience for everyone,
+              which is why we've curated a collection of delicious recipes that
+              are sure to delight your taste buds.
+            </p>
+            <div className="flex justify-center lg:block">
+              <Link to="/recipes" className="btn btn-secondary rounded-full">
+                Explore Recipes &#8594;
+              </Link>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
--- src/pages/login.jsx
+++ src/pages/login.jsx
@@ -1,3 +1,90 @@
-export default function login() {
-  return <h1>Login Page</h1>;
+import { useForm } from "react-hook-form";
+import { Link, useNavigate } from "react-router-dom";
+import InputErrorMessage from "../components/InputErrorMessage";
+import { useState } from "react";
+import { useAuth } from "../contexts/Auth";
+import loginImage from "../assets/login.webp";
+import TextInput from "../components/TextInput";
+
+export default function Login() {
+  const {
+    handleSubmit,
+    register,
+    formState: { isSubmitting },
+  } = useForm();
+  const [loginFailed, setLoginFailed] = useState(false);
+  const { signIn } = useAuth();
+  const navigate = useNavigate();
+
+  const onSubmit = async (data) => {
+    const { error } = await signIn({
+      email: data.email,
+      password: data.password,
+    });
+    if (error) {
+      setLoginFailed(true);
+    } else {
+      setLoginFailed(false);
+      navigate("/");
+    }
+  };
+
+  return (
+    <div className="flex h-screen w-screen items-center">
+      <div className="hidden md:block w-3/6 h-screen">
+        <img
+          src={loginImage}
+          alt="Login image"
+          className="object-cover h-full w-full"
+        />
+      </div>
+      <div className="w-full flex justify-center px-4">
+        <div className="lg:max-w-lg">
+          <h1 className="text-4xl font-bold">
+            Login to get full access to our website.
+          </h1>
+          <p className="my-2">
+            Login with your credentials to connect your account. You’ll be able
+            to sign in and access our website.
+          </p>
+          {loginFailed && (
+            <div className="bg-red-100 p-3 rounded-lg">
+              <InputErrorMessage message="Invalid email or password" />
+            </div>
+          )}
+          <form onSubmit={handleSubmit(onSubmit)}>
+            <div>
+              <TextInput
+                type="email"
+                label="Email"
+                name="email"
+                register={register("email")}
+              />
+            </div>
+            <div className="mb-2">
+              <TextInput
+                type="password"
+                label="Password"
+                name="password"
+                register={register("password")}
+              />
+            </div>
+            <p className="text-md  ">
+              Dont have any account?{" "}
+              <Link to={"/register"} className="hover:text-red-300 font-bold">
+                Register
+              </Link>
+            </p>
+            <button
+              className={`btn btn-md btn-secondary px-16 mt-4 ${
+                isSubmitting ? "loading" : ""
+              }`}
+            >
+              login
+            </button>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
 }
