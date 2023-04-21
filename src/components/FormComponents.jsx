bb8a9ef34ded2bc160e60338abdd9090f2d4b368
agusbw <agusbudhiarta81@gmial.com>
2023-04-21 13:16:00 +0800

    refactor: change FormComponents

--- src/components/FormComponents.jsx
+++ src/components/FormComponents.jsx
@@ -18,4 +18,15 @@ const TextInput = ({ label, name, type, register, className, ...rest }) => {
   );
 };
 
-export default TextInput;
+const Select = ({ className, children, ...rest }) => {
+  return (
+    <select
+      className={`${className} select bg-base-200 outline-none outline-offset-0 focus:outline-offset-0 hover:outline hover:outline-4 ease-in-out transition-all duration-100 hover:outline-red-200 hover:bg-white hover:border hover:border-gray-300 focus:border-red-300 focus:bg-white focus:outline-red-200 focus:outline-4 active:outline-0`}
+      {...rest}
+    >
+      {children}
+    </select>
+  );
+};
+
+export { TextInput, Select };
