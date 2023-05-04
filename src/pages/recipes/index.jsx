d0cd3df6250da0117dd17a2fcacfb22b928bf82e
agusbw <agusbudhiarta81@gmial.com>
2023-05-04 13:34:33 +0800

    fix: responsive issues

--- src/pages/recipes/index.jsx
+++ src/pages/recipes/index.jsx
@@ -114,7 +114,7 @@ export default function PublicRecipes() {
                 onChange={handleSearchChange}
               />
             </div>
-            <div className="flex lg:flex-row flex-col items-center mt-4 gap-4">
+            <div className="flex lg:flex-row flex-col mt-4 gap-4">
               <div className="collapse border border-base-300 bg-base-100 rounded-box collapse-arrow w-full md:w-fit">
                 <input type="checkbox" />
                 <div className="collapse-title text-md font-medium">
