9ae038cbca3a1bfb932437a574081f5a02c8490d
agusbw <agusbudhiarta81@gmial.com>
2023-05-09 13:13:22 +0800

    fix: empty div rendered when label is empty

--- src/components/FormComponents.jsx
+++ src/components/FormComponents.jsx
@@ -14,12 +14,14 @@ const TextInput = ({
 }) => {
   return (
     <>
-      <label
-        className="label text-base label-text font-semibold"
-        htmlFor={name}
-      >
-        <span className="label-text">{label}</span>
-      </label>
+      {label && (
+        <label
+          className="label text-base label-text font-semibold"
+          htmlFor={name}
+        >
+          <span className="label-text">{label}</span>
+        </label>
+      )}
       <input
         type={type}
         className={`${className} w-full input outline-none outline-offset-0 focus:outline-offset-0 hover:outline hover:outline-4 ease-in-out transition-all duration-100 hover:outline-red-200 hover:bg-white hover:border hover:border-gray-300 focus:border-red-300 focus:bg-white focus:outline-red-200 focus:outline-4 active:outline-0 ${variants[variant]}`}
